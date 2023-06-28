65cfde20148d7a25f325feef1574c5866ffe9b9d
Jason Cairns <jason.cairns@auckland.ac.nz>
2023-06-28 19:49:00 +1200

    initial program structure

---
+++ ng.h
@@ -0,0 +1,6 @@
+int traverse_files(char* dir);
+int record_connections(char *path);
+int is_note(char *path);
+int is_file(char *path);
+int is_directory(char *path);
+int ls(char *directory, char *dirpaths[]);
